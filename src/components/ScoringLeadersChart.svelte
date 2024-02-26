f35353dbf11e6d588b0a4744967f417339658794
Ansh Mujral <anshmujral30@gmail.com>
2024-02-26 11:34:52 -0800

    fixed data, added the data, added favicon, created scrollteller files and chart files

---
+++ src/components/ScoringLeadersChart.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+  
+    export let data = [];
+  
+    onMount(() => {
+      if (data.length) {
+        drawChart();
+      }
+    });
+  
+    function drawChart() {
+      // gotta fill this out
+    }
+  </script>
+  
+  <div id="chart"></div>
---
+++ src/components/ScrollyTeller.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { onMount } from 'svelte';
+    import ScoringLeadersChart from './ScoringLeadersChart.svelte';
+
+  </script>
